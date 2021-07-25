5b4923ca74b9001ad46e2db77de692ce154f10d3
Alex Leung <aleung@squareup.com>
2021-07-25 18:29:30 -0400

    Add dice image assets

--- DiceRoller/app/src/main/java/ca/alexleung/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/ca/alexleung/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package ca.alexleung.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
@@ -11,15 +11,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.roll_button)
+        val rollButton = findViewById<Button>(R.id.roll_button)
         rollButton.setOnClickListener {
             rollDice()
         }
     }
 
     private fun rollDice() {
-        val randomInt = Random().nextInt(6) + 1
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val diceImage = findViewById<ImageView>(R.id.dice_image)
+        val drawableResource = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }

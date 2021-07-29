d466cca2baa6039459a14e74fe80e29db6f66457
Alex Leung <aleung@squareup.com>
2021-07-28 22:41:11 -0400

    Replace view bindings with data bindings

--- AboutMe/app/src/main/java/ca/alexleung/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/ca/alexleung/aboutme/MainActivity.kt
@@ -4,32 +4,38 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import ca.alexleung.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName = MyName("Alex Leung")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
-            addNickname(it as Button)
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
         }
     }
 
-    private fun addNickname(doneButton: Button) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+    private fun addNickname(view: View) {
 
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        doneButton.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
         val inputMethodManager =
             getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.hideSoftInputFromWindow(doneButton.windowToken, 0)
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
---
+++ AboutMe/app/src/main/java/ca/alexleung/aboutme/MyName.kt
@@ -0,0 +1,6 @@
+package ca.alexleung.aboutme
+
+data class MyName(
+    var name: String = "",
+    var nickname: String = ""
+)

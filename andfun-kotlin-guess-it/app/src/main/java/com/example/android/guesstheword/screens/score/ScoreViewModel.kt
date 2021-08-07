f48d4d1da7fc62f7883d94466fea5c04123f5842
Alex Leung <aleung@squareup.com>
2021-08-07 18:01:41 -0400

    Introduce ScoreViewModel

---
+++ andfun-kotlin-guess-it/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.android.guesstheword.screens.score
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class ScoreViewModel(finalScore: Int) : ViewModel() {
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
+    init {
+        _score.value = finalScore
+    }
+
+    class Factory(private val finalScore: Int) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(ScoreViewModel::class.java)) {
+                return ScoreViewModel(finalScore) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
+}

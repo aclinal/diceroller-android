747066196393f8e5e00b7d9d9f88c033dd9f07c1
Alex Leung <aleung@squareup.com>
2021-08-15 16:18:14 -0400

    Use RecyclerView

--- andfun-kotlin-sleep-tracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ andfun-kotlin-sleep-tracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -61,6 +61,15 @@ class SleepTrackerFragment : Fragment() {
         binding.lifecycleOwner = this
         binding.sleepTrackerViewModel = sleepTrackerViewModel
 
+        val adapter = SleepNightAdapter()
+        binding.sleepList.adapter = adapter
+
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+        })
+
         sleepTrackerViewModel.navigateToSleepQualityEvent.observe(viewLifecycleOwner, Observer { sleepNight ->
             sleepNight?.let {
                 this.findNavController().navigate(
